48124a268e80ca344a642414962b2afee10a9d89
vagrant <vagrant@localhost.localdomain>
2019-11-06 12:06:44 +0900

    Finish user signup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -53,13 +53,13 @@ class UserTest < ActiveSupport::TestCase
     end
      
     test "password should be present (nonblank)" do
-      @user.password = @user.password_confirmation = " " * 6
-      assert_not @user.valid?
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
     end
 
     test "password should have a minimum length" do
-      @user.password = @user.password_confirmation = "a" * 5
-      assert_not @user.valid?
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
     end
 
 end
